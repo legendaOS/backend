97234c526cd4991b22c5a8e40736c0cae82c6747
FedOS <76245715+legendaOS@users.noreply.github.com>
2024-12-05 15:44:29 +0300

    fix 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env?.port ? process.env?.port : 3000
 const domain = process.env?.domain ? process.env?.domain : "agoi_sochi"
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://roolegenda.ru");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
