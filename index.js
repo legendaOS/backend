269670012dbb036c6e2cf024cc38d0583b480000
FedOS <76245715+legendaOS@users.noreply.github.com>
2024-12-05 16:28:12 +0300

    123123121231212323123123

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const port = process.env?.port ? process.env?.port : 3000
 const domain = process.env?.domain ? process.env?.domain : "agoi_sochi"
 
 app.use(function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.header("Access-Control-Allow-Credentials", true);
-    // res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PATCH");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PATCH");
     next();
 });
 
