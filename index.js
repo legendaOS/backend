a9ff4dd850cad343a81dd8323d74774bc68e3a77
FedOS <76245715+legendaOS@users.noreply.github.com>
2024-12-05 16:03:10 +0300

    fix

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const domain = process.env?.domain ? process.env?.domain : "agoi_sochi"
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 });
 
